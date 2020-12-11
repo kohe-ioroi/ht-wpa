eec79fb29a228a520a01728c1aaa3f0f233c06c5
DESKTOP-QP9SN0I\ioroi <ioroikouhei64@gmail.com>
2020-12-11 12:30:13 +0900

    no message

--- js/main.js
+++ js/main.js
@@ -83,20 +83,20 @@ const startScanner = () => {
     });
     const calc = isbn => {
         const arrIsbn = isbn
-          .toString()
-          .split("")
-          .map(num => parseInt(num));
+            .toString()
+            .split("")
+            .map(num => parseInt(num));
         let remainder = 0;
         const checkDigit = arrIsbn.pop();
-      
+        
         arrIsbn.forEach((num, index) => {
-          remainder += num * (index % 2 === 0 ? 1 : 3);
+            remainder += num * (index % 2 === 0 ? 1 : 3);
         });
         remainder %= 10;
         remainder = remainder === 0 ? 0 : 10 - remainder;
-      
+        
         return checkDigit === remainder;
-      }
+        }
       
     //barcode read call back
     Quagga.onDetected(success => {
