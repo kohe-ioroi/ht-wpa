732761d6a9eb7304f90c5b4169b02e62bbf41418
DESKTOP-QP9SN0I\ioroi <ioroikouhei64@gmail.com>
2020-12-11 10:59:11 +0900

    別のソース丸パクリ。

--- js/main.js
+++ js/main.js
@@ -1,78 +1,52 @@
-$(function () {
-
-    startScanner();
-
-});
-
-const startScanner = () => {
-    Quagga.init({
-        inputStream: {
-          name: 'Live',
-          type: 'LiveStream',
-          target: document.querySelector('#photo-area'), //埋め込んだdivのID
-          constraints: {
-            facingMode: 'environment',
-          },
-        },
-        locator: {
-          patchSize: 'medium',
-          halfSample: true,
-        },
-        numOfWorkers: 2,
-        decoder: {
-          readers: ['ean_reader'] //ISBNは基本的にこれ（他にも種類あり）
-        },
-        locate: true,
-      }, (err) => {
-        if(!err) {
-          Quagga.start();
-        }
-      })}
-
-    Quagga.onProcessed(function (result) {
-        var drawingCtx = Quagga.canvas.ctx.overlay,
-            drawingCanvas = Quagga.canvas.dom.overlay;
-
-        if (result) {
-            if (result.boxes) {
-                drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
-                result.boxes.filter(function (box) {
-                    return box !== result.box;
-                }).forEach(function (box) {
-                    Quagga.ImageDebug.drawPath(box, {
-                        x: 0,
-                        y: 1
-                    }, drawingCtx, {
-                        color: "green",
-                        lineWidth: 2
-                    });
-                });
-            }
-
-            if (result.box) {
-                Quagga.ImageDebug.drawPath(result.box, {
-                    x: 0,
-                    y: 1
-                }, drawingCtx, {
-                    color: "#00F",
-                    lineWidth: 2
-                });
-            }
-
-            if (result.codeResult && result.codeResult.code) {
-                Quagga.ImageDebug.drawPath(result.line, {
-                    x: 'x',
-                    y: 'y'
-                }, drawingCtx, {
-                    color: 'red',
-                    lineWidth: 3
-                });
-            }
-        }
+Quagga.init({
+    inputStream: {
+      name: 'Live',
+      type: 'LiveStream',
+      target: document.querySelector('#interactive'),//埋め込んだdivのID
+      constraints: {
+        facingMode: 'environment',
+      },
+      area: {//必要ならバーコードの読み取り範囲を調整できる（この場合は、上30%/下30%は読み取りしない）
+        top: "30%",
+        right: "0%",
+        left: "0%",
+        bottom: "30%"
+      },
+    },
+    locator: {
+      patchSize: 'medium',
+      halfSample: true,
+    },
+    numOfWorkers: 2,
+    decoder: {
+      readers: ['ean_reader']//ISBNは基本的にこれ（他にも種類あり）
+    },
+    locate: true,
+  }, (err) => {
+    if(!err) {
+      Quagga.start();
+      // alert("started");
+    }
+  })
+  
+  Quagga.onDetected(success => {
+    const code = success.codeResult.code;
+    if(calc(code)) alert(code);
+  })
+  
+  const calc = isbn => {
+    const arrIsbn = isbn
+      .toString()
+      .split("")
+      .map(num => parseInt(num));
+    let remainder = 0;
+    const checkDigit = arrIsbn.pop();
+  
+    arrIsbn.forEach((num, index) => {
+      remainder += num * (index % 2 === 0 ? 1 : 3);
     });
-
-    //barcode read call back
-    Quagga.onDetected(success => {
-        const code = success.codeResult.code;
-        alert(code);
-      })
+    remainder %= 10;
+    remainder = remainder === 0 ? 0 : 10 - remainder;
+  
+    return checkDigit === remainder;
+  }
