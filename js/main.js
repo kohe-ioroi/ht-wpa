ba308b35f6a1c46067ce7e6280bbda4172bfad7d
DESKTOP-QP9SN0I\ioroi <ioroikouhei64@gmail.com>
2020-12-11 12:32:30 +0900

    no message

--- js/main.js
+++ js/main.js
@@ -81,26 +81,9 @@ const startScanner = () => {
             }
         }
     });
-    const calc = isbn => {
-        const arrIsbn = isbn
-            .toString()
-            .split("")
-            .map(num => parseInt(num));
-        let remainder = 0;
-        const checkDigit = arrIsbn.pop();
-        
-        arrIsbn.forEach((num, index) => {
-            remainder += num * (index % 2 === 0 ? 1 : 3);
-        });
-        remainder %= 10;
-        remainder = remainder === 0 ? 0 : 10 - remainder;
-        
-        return checkDigit === remainder;
-        }
-      
+
     //barcode read call back
-    Quagga.onDetected(success => {
-        const code = success.codeResult.code;
-        if(calc(code)) alert(code);
-      })
+    Quagga.onDetected(function (result) {
+        alert(result.codeResult.code);
+    });
 }
