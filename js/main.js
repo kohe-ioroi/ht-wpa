f7a3c696a20860faa9b34508a8c36a98c7874a54
DESKTOP-QP9SN0I\ioroi <ioroikouhei64@gmail.com>
2020-12-11 22:09:32 +0900

    1.4.6

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(function () {
-    alert("V1.4.5")
+    alert("V1.4.6")
     startScanner();
 });
 
@@ -23,7 +23,7 @@ function displayData(data) {
 }
 
 function ajaxerror(){
-    alert("エラー！多分DBにありません...");
+    alert("エラー！DBに無いか、接続エラーです。");
     startScanner();
 }
 
@@ -39,8 +39,8 @@ const startScanner = () => {
                 codeRepetition: true,
                 tryVertical: true,
                 frameRate: 15,
-                width: 1280,
-                height: 720,
+                width: 640,
+                height: 480,
                 facingMode: "environment"
             },
         },
@@ -62,47 +62,7 @@ const startScanner = () => {
         _scannerIsRunning = true;
     });
 
-    Quagga.onProcessed(function (result) {
-        var drawingCtx = Quagga.canvas.ctx.overlay,
-            drawingCanvas = Quagga.canvas.dom.overlay;
-
-        if (result) {
-            if (result.boxes) {
-                drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
-                result.boxes.filter(function (box) {
-                    return box !== result.box;
-                }).forEach(function (box) {
-                    Quagga.ImageDebug.drawPath(box, {
-                        x: 0,
-                        y: 1
-                    }, drawingCtx, {
-                        color: "green",
-                        lineWidth: 2
-                    });
-                });
-            }
-
-            if (result.box) {
-                Quagga.ImageDebug.drawPath(result.box, {
-                    x: 0,
-                    y: 1
-                }, drawingCtx, {
-                    color: "#00F",
-                    lineWidth: 2
-                });
-            }
-
-            if (result.codeResult && result.codeResult.code) {
-                Quagga.ImageDebug.drawPath(result.line, {
-                    x: 'x',
-                    y: 'y'
-                }, drawingCtx, {
-                    color: 'red',
-                    lineWidth: 3
-                });
-            }
-        }
-    });
+    Quagga.onProcessed(function (result) {});
 
     Quagga.onDetected(function (result) {
         var code = result.codeResult.code;
