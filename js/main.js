a8591da650b9804bb0fb06ee6abb8e5bf61356d0
DESKTOP-QP9SN0I\ioroi <ioroikouhei64@gmail.com>
2020-12-14 00:27:40 +0900

    大規模改変

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
+var scancount = 0
+var scandata = []
 $(function () {
-    alert("V1.5.7\nこれは試験版です。")
+    alert("V1.5.8\nこれは試験版です。")
     startScanner();
 });
 
@@ -15,16 +17,18 @@ function sendRequest(code) {
     error: function(){ajaxerror()}
     });
 }
-
+function sendRequest_input(){
+    sendRequest(document.getElementById("jancode").value);
+    document.getElementById("jancode").value = "";
+}
 function displayData(data) {
     arrayd=data.split(',');
     pbcheck="X";
     if(arrayd[4]){pbcheck="O"}
-    alert("部門:"+arrayd[0]+"\nJANコード:"+arrayd[1]+"\n商品名:"+arrayd[2]+"\nPB判定:"+pbcheck+"\n台番:"+arrayd[5]+"\n列:"+arrayd[6]+"\n行:"+arrayd[7]);
+    alert("部門:"+arrayd[0]+"\nJANコード:"+arrayd[1]+"\n商品名:"+arrayd[2]+"\nPB判定:"+pbcheck+"\n台番:"+arrayd[5]+" 段:"+arrayd[6]+" 列:"+arrayd[7]);
     showmain();
     startScanner();
 }
-
 function ajaxerror(){
     alert("エラー！\nDBにデータが無いか、不正なコードです。\nもう一度読み取りして下さい。");
     showmain();
@@ -54,7 +58,45 @@ function calc(isbn) {
 
     return checkDigit === remainder;
 }
+Array.prototype.mode = function () {
+    if (this.length === 0){
+        //配列の個数が0だとエラーを返す。
+        throw new Error("配列の長さが0のため最頻値が計算できません");
+        //nullを返しても困らない時(配列の中にnullが無い時)はnullを返すように実装しても良い。
+        //return null
+    }
+    //回数を記録する連想配列
+    var counter = {}
+    //本来の値を入れた辞書
+    var nativeValues = {}
+
+    //最頻値とその出現回数を挿入する変数
+    var maxCounter = 0;
+    var maxValue = null;
+
+    for (var i = 0; i < this.length; i++) {
+        //counterに存在しなければ作る。keyは型を区別する
+        if (!counter[this[i] + "_" + typeof this[i]]) {
+            counter[this[i] + "_" + typeof this[i]] = 0;
+        }
+        counter[this[i] + "_" + typeof this[i]]++;
+        nativeValues[this[i] + "_" + typeof this[i]] = this[i];
+
+    }
+    for (var j = 0; j < Object.keys(counter).length; j++) {
+        key = Object.keys(counter)[j];
+        if (counter[key] > maxCounter) {
+            maxCounter = counter[key];
+            maxValue = nativeValues[key]
+        }
+    }
+    return maxValue
+
+}
 function startScanner() {
+    scancount = 0
+    scandata = []
+    document.getElementById("scanprogress").value(scancount);
     Quagga.init({
         inputStream: {
             name: "Live",
@@ -65,9 +107,9 @@ function startScanner() {
                 successTimeout: 500,
                 codeRepetition: true,
                 tryVertical: true,
-                frameRate: 15,
-                width: 640,
-                height: 480,
+                frameRate: 10,
+                width: 1280,
+                height: 960,
                 facingMode: "environment"
             },
         },
@@ -78,7 +120,7 @@ function startScanner() {
         },
         locator:{
             halfSample: true,
-            patchSize: "small",
+            patchSize: "x-small",
         }
     }, function (err) {
         if (err) {
@@ -97,11 +139,19 @@ function startScanner() {
     Quagga.onDetected(function (result) {
         var code = result.codeResult.code;
         if (calc(code)) {
-            showload();
-            Quagga.offProcessed();
-            Quagga.offDetected();
-            Quagga.stop();
-            sendRequest(code);
+            if (scancount < 10){
+                document.getElementById("scanprogress").value(scancount);
+                scandata.push(code);
+            }
+            else if (scancount == 10){
+                document.getElementById("scanprogress").value(scancount);
+                Quagga.offProcessed();
+                Quagga.offDetected();
+                Quagga.stop();
+                showload();
+                sendRequest(scandata.mode());
+            }
+            
         };
     });
 }
